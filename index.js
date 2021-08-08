999e5cae79f69704aaa4ec2c824ed000671d6757
mukhriddin <khodievm@gmail.com>
2021-08-08 11:42:27 +0500

    ok

---
+++ index.js
@@ -0,0 +1,22 @@
+// document.querySelectorAll("h1").forEach(function (value, index) {
+//     value.onclick = function () {
+//         let y = this.classList.contains("d"){
+
+//         };
+//         console.log(y);
+//     };
+// });
+
+
+
+// document.querySelectorAll("h1").forEach(function (value, index) {
+//     value.onclick = function (e) {
+//         console.log(e.target.nodeName);
+
+//     };
+// });
+
+
+// document.myform.finsh.onblur = function () {
+//     this.type = "password";
+// };
